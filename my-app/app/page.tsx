6f51de6b7e1ccbffc215102af386a9f13ce46f4a
Boon Juan <boonjuan1@gmail.com>
2023-04-03 12:07:03 +0800

    First commit

---
+++ my-app/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="text-red-500 font-bold flex justify-center items-center h-screen font-poppin">
+      Hello
+    </main>
+  );
+}
